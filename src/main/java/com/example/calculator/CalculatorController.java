ea9c327c711f49271b4837c3a0d9fa818b6bb5ae
Jack Chung <jackchung@jfrogchina.com>
2019-10-28 17:04:06 +0800

    Added junit dependency to the pom file

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,4 +1,4 @@
-package com.leszko.calculator;
+package com.example.calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +9,9 @@ class CalculatorController {
 @Autowired
 private Calculator calculator;
 
-@RequestMapping(“/sum”)
-String sum(@RequestParam(“a”) Integer a,
-@RequestParam(“b”) Integer b) {
+@RequestMapping("/sum")
+String sum(@RequestParam("a") Integer a,
+@RequestParam("b") Integer b) {
 return String.valueOf(calculator.sum(a, b));
 }
 }
